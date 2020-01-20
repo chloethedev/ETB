0731b9ee7b51446af4f114ee76fc180582c667e7
chloethedev <chloethedev@gmail.com>
2020-01-20 11:32:26 -0500

    Completed the tests for the contract for string manipulation. It was quite simple tests. Starting to get the hang of testing...

--- day11-string-manipulation/tests/test/string-manipulation.js
+++ day11-string-manipulation/tests/test/string-manipulation.js
@@ -5,5 +5,15 @@ contract('Strings', () => {
   before(async () => {
     strings = await Strings.deployed();
   });
+
+  it('Should return the length of a string', async () => {
+    const length = await strings.length('abc');
+    assert(length.toNumber() === 3);
+  });
+
+  it('Should concatenate 2 strings', async () => {
+    const concatenatedString = await strings.concatenate('abc', 'def');
+    assert(concatenatedString === 'abcdef');
+  })
 });
 
