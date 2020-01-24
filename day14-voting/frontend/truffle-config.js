a86ccc57a7807b06d98a9fce6d736c51468abdc3
chloethedev <chloethedev@gmail.com>
2020-01-24 17:48:19 -0500

    Set up for DAO project.

--- day14-voting/frontend/truffle-config.js
+++ day14-voting/frontend/truffle-config.js
@@ -1,4 +1,13 @@
 const path = require("path");
+const fs = require('fs');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const secrets = JSON.parse(
+  fs.readFileSync(".secrets").toString().trim()
+);
+
+
+
+
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -10,10 +19,18 @@ module.exports = {
     }
   },
   networks: {
-    ganache: {
-      host: "localhost",
-      port: 7545,
-      network_id: "*" // Match any network id
-    }
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(secrets.seed, "https://ropsten.infura.io/v3/${secrets.projectId}");
+      },
+      network_id: '3',
+    },
+    test: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
+      },
+      network_id: '*',
+    },
   }
 };
+
